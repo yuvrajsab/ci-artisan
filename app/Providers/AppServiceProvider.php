ccb174b5c61c97e2f48d1c93a5f0d4056f1b6c16
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-10-28 11:03:30 +0530

    add finding paths logic to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (! File::isFile(base_path(config('settings.entry_point')))) {
+            exit(config('settings.entry_point').' not found');
+        }
+
+        $contents = File::get(base_path(config('settings.entry_point')));
+
+        $matches = [];
+        preg_match_all(
+            '~\$([a-zA-Z_][a-zA-Z0-0_]*) *= *[\'"]([^\'"]*)[\'"]~',
+            $contents,
+            $matches
+        );
+
+        $vars = array_combine($matches[1], $matches[2]);
+
+        if (array_key_exists('application_folder', $vars)) {
+            config('settings.controllers_path', $vars["application_folder"].'/controllers/');
+            config('settings.models_path', $vars["application_folder"].'/models/');
+        } else {
+            exit("'application' folder not found");
+        }
     }
 
     /**
