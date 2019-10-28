e8931c55fbc2259019f17331d5ea4fce77aca2af
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-10-28 11:05:21 +0530

    remove unnecessary stuff

--- config/settings.php
+++ config/settings.php
@@ -2,11 +2,8 @@
 
 return [
 
-    'controllers_path' => env('CONTROLLERS_PATH', 'application/controllers/'),
-    'models_path' => env('MODELS_PATH', 'application/models/'),
-
-    'base_controller' => env('BASE_CONTROLLER', 'CI_Controller'),
-    'base_model' => env('BASE_MODEL', 'CI_Model'),
+    'controllers_path' => 'application/controllers/',
+    'models_path' => 'application/models/',
     
-    'entry_point' => env('ENTRY_POINT', 'index.php'),
+    'entry_point' => 'index.php',
 ];
