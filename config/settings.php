8df91f2b988d95462acc73d860f210fb65d322c2
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-10-28 21:28:08 +0530

    add third party path in config file

--- config/settings.php
+++ config/settings.php
@@ -6,5 +6,6 @@ return [
     'models_path' => 'application/models/',
     'migrations_path' => 'application/migrations/',
     'tests_path' => 'application/controllers/tests/',
+    'third_party_path' => 'application/third_party/',
     'entry_point' => env('CI_ARTISAN_ENTRY_POINT', 'index.php'),
 ];
