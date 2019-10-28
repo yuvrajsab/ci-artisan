09b8bb6440232ecf5bc0b0ee2ab9ce1200a438b6
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-10-28 11:10:40 +0530

    remove base controller feature

--- stubs/controller/simple_controller_template.php
+++ stubs/controller/simple_controller_template.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class {$controller_name} extends {$base_controller}
+class {$controller_name} extends CI_Controller
 {
     // 
 }
