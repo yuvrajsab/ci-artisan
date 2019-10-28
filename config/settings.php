1b2e8a962cab9b86db2b130f3cd211a0774311e4
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-10-28 18:01:26 +0530

    add dotenv for entry point (required)

--- config/settings.php
+++ config/settings.php
@@ -5,5 +5,5 @@ return [
     'controllers_path' => 'application/controllers/',
     'models_path' => 'application/models/',
     'migrations_path' => 'application/migrations/',
-    'entry_point' => 'index.php',
+    'entry_point' => env('CI_ARTISAN_ENTRY_POINT', 'index.php'),
 ];
