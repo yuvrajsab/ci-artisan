b58defe98a5d6fb3c341f9f86ae713dd201c2a5d
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-10-31 15:14:37 +0530

    add views path in config

--- config/settings.php
+++ config/settings.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-
+    'config_path' => 'application/config/',
     'controllers_path' => 'application/controllers/',
     'models_path' => 'application/models/',
     'migrations_path' => 'application/migrations/',
     'tests_path' => 'application/controllers/tests/',
     'third_party_path' => 'application/third_party/',
+    'views_path' => 'application/views/',
     'entry_point' => env('CI_ARTISAN_ENTRY_POINT', 'index.php'),
 ];
